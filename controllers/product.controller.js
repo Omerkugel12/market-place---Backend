3880245943da2e0a09a821fa3b789ffc03fe5229
Omerkugel12 <omerkugel12@gmail.com>
2024-06-26 00:38:14 +0300

    criteria-filter-maxPrice

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -10,6 +10,17 @@ function buildCriteria(query) {
   if (query.category) {
     criteria.category = { $regex: query.category, $options: "i" };
   }
+  if (query.minPrice && !isNaN(query.minPrice)) {
+    query.minPrice = parseFloat(query.minPrice);
+    criteria.price = { $gte: query.minPrice };
+  }
+  if (query.maxPrice && !isNaN(query.maxPrice)) {
+    query.maxPrice = parseFloat(query.maxPrice);
+    if (!criteria.price) {
+      criteria.price = {};
+    }
+    criteria.price.$lte = query.maxPrice;
+  }
   return criteria;
 }
 
