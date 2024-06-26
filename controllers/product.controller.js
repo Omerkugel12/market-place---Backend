b66e96b461a109c93fb2ef4e3c98979544702030
Omerkugel12 <omerkugel12@gmail.com>
2024-06-26 10:41:56 +0300

    helpers

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,28 +1,6 @@
 // const { query } = require("express");
+const { buildCriteria } = require("../helpers/product.helper");
 const Product = require("../models/products.model");
-
-function buildCriteria(query) {
-  const criteria = {};
-
-  if (query.name) {
-    criteria.name = { $regex: query.name, $options: "i" };
-  }
-  if (query.category) {
-    criteria.category = { $regex: query.category, $options: "i" };
-  }
-  if (query.minPrice && !isNaN(query.minPrice)) {
-    query.minPrice = parseFloat(query.minPrice);
-    criteria.price = { $gte: query.minPrice };
-  }
-  if (query.maxPrice && !isNaN(query.maxPrice)) {
-    query.maxPrice = parseFloat(query.maxPrice);
-    if (!criteria.price) {
-      criteria.price = {};
-    }
-    criteria.price.$lte = query.maxPrice;
-  }
-  return criteria;
-}
 
 async function getProductsCount(req, res) {
   const name = req.query.name || "";
--- helpers/product.helper.js
+++ helpers/product.helper.js
@@ -1,7 +1,26 @@
-// function buildCriteria(query){
-//     const criteria = {}
+function buildCriteria(query) {
+  const criteria = {};
 
-//     if (query.name) {
-//         criteria.name = ($regex:query.name, )
-//     }
-// }
+  if (query.name) {
+    criteria.name = { $regex: query.name, $options: "i" };
+  }
+  if (query.category) {
+    criteria.category = { $regex: query.category, $options: "i" };
+  }
+  if (query.minPrice && !isNaN(query.minPrice)) {
+    query.minPrice = parseFloat(query.minPrice);
+    criteria.price = { $gte: query.minPrice };
+  }
+  if (query.maxPrice && !isNaN(query.maxPrice)) {
+    query.maxPrice = parseFloat(query.maxPrice);
+    if (!criteria.price) {
+      criteria.price = {};
+    }
+    criteria.price.$lte = query.maxPrice;
+  }
+  return criteria;
+}
+
+module.exports = {
+  buildCriteria,
+};
